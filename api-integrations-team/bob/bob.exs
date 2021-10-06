b6482335b2c41b32a940d0e47f9176df33f38c0a
Sam Wood <sam@zappistore.com>
2021-10-06 13:58:47 +0100

    Sam's first commit in 2 years

--- api-integrations-team/bob/bob.exs
+++ api-integrations-team/bob/bob.exs
@@ -3,8 +3,11 @@ defmodule Teenager do
     cond do
       # _ when String.ends_with(input, "?") -> "Sure."
       # _ when String.trim(input) == "" -> "Fine. Be that way!"
+      String.ends_with?(input, "?") -> "Sure."
+      String.trim(input) == "" -> "Fine. Be that way!"
       input == String.upcase(input) ->
         "Whoa, chill out!"
+      true -> "Whatever."
         # _ -> "Whatever."
     end
   end

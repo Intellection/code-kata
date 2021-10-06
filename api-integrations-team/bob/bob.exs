d4140deeb359959207f94d2f8f084a9bd3dd6c4f
Joe Bolt <josef.m.bolt@gmail.com>
2021-10-06 13:39:08 +0100

    bob

--- api-integrations-team/bob/bob.exs
+++ api-integrations-team/bob/bob.exs
@@ -1,4 +1,10 @@
 defmodule Teenager do
   def hey(input) do
+    case input do
+      _ when String.ends_with("?") -> "Sure."
+      _ when String.trim(input) == "" -> "Fine. Be that way!"
+      String.upcase(input) -> "Whoa, chill out!"
+      _ -> "Whatever."
+    end
   end
 end

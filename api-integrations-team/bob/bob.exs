26fe867504ce6521d06b0a0e99ba7d8affee8f5f
Ben Mmari <benmmari92@gmail.com>
2021-10-06 14:49:30 +0200

    lol

--- api-integrations-team/bob/bob.exs
+++ api-integrations-team/bob/bob.exs
@@ -1,10 +1,11 @@
 defmodule Teenager do
   def hey(input) do
-    case input do
-      _ when String.ends_with("?") -> "Sure."
-      _ when String.trim(input) == "" -> "Fine. Be that way!"
-      String.upcase(input) -> "Whoa, chill out!"
-      _ -> "Whatever."
+    cond do
+      # _ when String.ends_with(input, "?") -> "Sure."
+      # _ when String.trim(input) == "" -> "Fine. Be that way!"
+      input == String.upcase(input) ->
+        "Whoa, chill out!"
+        # _ -> "Whatever."
     end
   end
 end
--- api-integrations-team/bob/bob_test.exs
+++ api-integrations-team/bob/bob_test.exs
@@ -4,7 +4,7 @@ else
   Code.load_file("bob.exs")
 end
 
-ExUnit.start
+ExUnit.start()
 ExUnit.configure(exclude: :pending)
 
 defmodule TeenagerTest do
@@ -14,67 +14,54 @@ defmodule TeenagerTest do
     assert Teenager.hey("Tom-ay-to, tom-aaaah-to.") == "Whatever."
   end
 
-  @tag :pending
   test "shouting" do
     assert Teenager.hey("WATCH OUT!") == "Whoa, chill out!"
   end
 
-  @tag :pending
   test "asking a question" do
     assert Teenager.hey("Does this cryogenic chamber make me look fat?") == "Sure."
   end
 
-  @tag :pending
   test "talking forcefully" do
     assert Teenager.hey("Let's go make out behind the gym!") == "Whatever."
   end
 
-  @tag :pending
   test "talking in capitals" do
     assert Teenager.hey("This Isn't Shouting!") == "Whatever."
   end
 
-  @tag :pending
   test "shouting numbers" do
     assert Teenager.hey("1, 2, 3 GO!") == "Whoa, chill out!"
   end
 
-  @tag :pending
   test "shouting with special characters" do
     assert Teenager.hey("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!") == "Whoa, chill out!"
   end
 
-  @tag :pending
   test "shouting with no exclamation mark" do
     assert Teenager.hey("I HATE YOU") == "Whoa, chill out!"
   end
 
-  @tag :pending
   test "statement containing question mark" do
     assert Teenager.hey("Ending with ? means a question.") == "Whatever."
   end
 
-  @tag :pending
   test "silence" do
     assert Teenager.hey("") == "Fine. Be that way!"
   end
 
-  @tag :pending
   test "prolonged silence" do
     assert Teenager.hey("  ") == "Fine. Be that way!"
   end
 
-  @tag :pending
   test "only numbers" do
     assert Teenager.hey("1, 2, 3") == "Whatever."
   end
 
-  @tag :pending
   test "question with numbers" do
     assert Teenager.hey("4?") == "Sure."
   end
 
-  @tag :pending
   test "shouting in Russian" do
     # Hopefully this is Russian for "get out"
     assert Teenager.hey("УХОДИТЬ") == "Whoa, chill out!"

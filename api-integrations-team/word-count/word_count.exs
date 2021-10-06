a67d01f9c43f4c87619b12ec09f570e17afce9a5
Ben Mmari <benmmari92@gmail.com>
2021-10-06 15:38:06 +0200

    almost

--- api-integrations-team/word-count/word_count.exs
+++ api-integrations-team/word-count/word_count.exs
@@ -11,6 +11,17 @@ defmodule Words do
     |> build_count_map()
   end
 
+  # we want to add each word to a map
+  # with the key being the word
+  # and the value being the number of iterations
   defp build_count_map(word_list) do
+    word_list
+    |> Enum.reduce(%{}, &upsert/2)
+  end
+
+  defp upsert(word, acc) do
+    # if it exists get it and add 1, if not its 1
+    value = acc[word]
+    %{word => 1}
   end
 end

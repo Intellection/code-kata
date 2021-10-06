2ed2ac7dcbbf4f72241eb1a528eaf14937a74f45
Jones Matse <jones.matse@zappistore.com>
2021-10-06 15:16:56 +0200

    Jones: I did a thing

--- api-integrations-team/bob/bob.exs
+++ api-integrations-team/bob/bob.exs
@@ -1,13 +1,17 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-      # _ when String.ends_with(input, "?") -> "Sure."
-      # _ when String.trim(input) == "" -> "Fine. Be that way!"
-      String.ends_with?(input, "?") -> "Sure."
-      String.trim(input) == "" -> "Fine. Be that way!"
-      input == String.upcase(input) ->
+      String.ends_with?(input, "?") ->
+        "Sure."
+
+      String.trim(input) == "" ->
+        "Fine. Be that way!"
+
+      input == String.upcase(input) and String.match?(input, ~r/\p{L}/) ->
         "Whoa, chill out!"
-      true -> "Whatever."
+
+      true ->
+        "Whatever."
         # _ -> "Whatever."
     end
   end

847cabf9bce24d797a706d8e0764205864d43e08
Jones Matse <jones.matse@zappistore.com>
2021-10-06 16:47:22 +0200

    Jones: elixir and ben and joe are showing me flames:

--- api-integrations-team/run-length-encoding/rle.exs
+++ api-integrations-team/run-length-encoding/rle.exs
@@ -8,5 +8,20 @@ defmodule RunLengthEncoder do
   """
   @spec encode(String.t()) :: String.t()
   def encode(string) do
+    string
+    |> String.to_char_list()
+    |> Enum.reduce([], &list_builder/2)
+  end
+
+  defp list_builder(elem, [{elem, count} | tail]) do
+    [{elem, count(+1)} | tail]
+  end
+
+  defp list_builder(elem, []) do
+    [{elem, 1}]
+  end
+
+  defp list_builder(elem, arr) do
+    [{elem, 1}]
   end
 end

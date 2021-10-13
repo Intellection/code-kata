e38f079590c9c7d2f21f447b3c258b046d8d2b2d
Sam Wood <sam@zappistore.com>
2021-10-13 14:01:09 +0100

    Mexican wave

--- api-integrations-team/round 2/mexican-wave/mexican_wave.ex
+++ api-integrations-team/round 2/mexican-wave/mexican_wave.ex
@@ -1,9 +1,15 @@
 defmodule MexicanWaveTests do
   def wave(word) do
-    word
-    |> String.to_charlist()
-    |>
+ #   additional_year = fn initial ->
+  #     initial * (1 + percentage_change * 0.01) + new_inhabitants
+  #   end
+    capitalise_letter = fn index
+      # capitalised_word = word.capitalise_at(index)
+      capitalised_word = word
+    end
 
+    Enum.to_list(0..String.length(word))
+    |> Enum.map(capitalise_letter)
 
   end
 end
--- api-integrations-team/round 2/mexican-wave/mexican_wave_tests.exs
+++ api-integrations-team/round 2/mexican-wave/mexican_wave_tests.exs
@@ -10,7 +10,4 @@ defmodule MexicanWaveTests do
     assert MexicanWave.wave("hello") == ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
   end
 
-  test "testing the wave with `Round 2`" do
-    assert PopulationGrowth.calculator(1_500_000, 2.5, 10000, 2_000_000) == 10
-  end
 end

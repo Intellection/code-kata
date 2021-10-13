008d774f46ff3d84d26898f6ed1b966bd7a21bea
Sam Wood <sam@zappistore.com>
2021-10-13 13:15:49 +0100

    Added stream and method

--- api-integrations-team/round 2/population-growth/population_growth.ex
+++ api-integrations-team/round 2/population-growth/population_growth.ex
@@ -1,4 +1,14 @@
 defmodule PopulationGrowth do
-  def calculator(one, two, three, four) do
+  def calculator(initial, percentage_change, new_inhabitants, final) do
+    additional_year = fn initial ->
+      initial * (1 + percentage_change * 0.01) + new_inhabitants
+    end
+    # Loop until initial > final
+    #     (initial = intitial + intitial*percentage + new_inhabitants)
+    Stream.iterate(initial, additional_year())
+  end
+
+  def additional_year(initial)
+  end
   end
 end

b9731f523c41eb433e06955b5a37eada7eccf42c
Joe Bolt <josef.m.bolt@gmail.com>
2021-10-13 13:26:26 +0100

    yo

--- api-integrations-team/round 2/population-growth/population_growth.ex
+++ api-integrations-team/round 2/population-growth/population_growth.ex
@@ -1,14 +1,15 @@
 defmodule PopulationGrowth do
-  def calculator(initial, percentage_change, new_inhabitants, final) do
-    additional_year = fn initial ->
-      initial * (1 + percentage_change * 0.01) + new_inhabitants
-    end
-    # Loop until initial > final
-    #     (initial = intitial + intitial*percentage + new_inhabitants)
-    Stream.iterate(initial, additional_year())
-  end
+  # def calculator(initial, percentage_change, new_inhabitants, final) do
+  #   additional_year = fn initial ->
+  #     initial * (1 + percentage_change * 0.01) + new_inhabitants
+  #   end
 
-  def additional_year(initial)
-  end
-  end
+  #   {pop, year} = initial
+  #                |> Stream.iterate(additional_year)
+  #                |> Stream.with_index()
+  #                |> Enum.find(fn {pop, index} -> pop > final end)
+
+  #   year
+  # end
+  def calculator(initial, percentage_change, new_inhabitants, final, day \\ 0) when initial > final, do:
 end

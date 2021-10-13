07e29750f205d9d83dd0b4b5f24bd553690c5368
Joe Bolt <josef.m.bolt@gmail.com>
2021-10-13 14:42:29 +0100

    gl

--- api-integrations-team/round 2/reverse-rotate/reverse_rotate.ex
+++ api-integrations-team/round 2/reverse-rotate/reverse_rotate.ex
@@ -1,10 +1,19 @@
 defmodule ReverseRotate do
   def rr(num, sz) do
     num
-    # |> String.codepoints()
-    |> Enum.map(fn letter -> letter end)
+    |> String.to_integer()
+    |> Integer.digits()
     |> Enum.chunk_every(sz, :discard)
-    # |>
-
+    |> Enum.map(&rotate/1)
+    |> Enum.map(&Integer.to_string/1)
+    |> String.join()
   end
- end
+
+  def rotate(nums = [h | t]) do
+    if rem(Enum.sum(nums), 2) == 0 do
+      Enum.reverse(nums)
+    else
+      t ++ [h]
+    end
+  end
+end

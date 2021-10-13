0b77d982327252c7f42ef450e594c263d619c5a0
Joe Bolt <josef.m.bolt@gmail.com>
2021-10-13 14:15:53 +0100

    mexico

--- api-integrations-team/round 2/mexican-wave/mexican_wave.ex
+++ api-integrations-team/round 2/mexican-wave/mexican_wave.ex
@@ -1,15 +1,13 @@
-defmodule MexicanWaveTests do
+defmodule MexicanWave do
   def wave(word) do
- #   additional_year = fn initial ->
-  #     initial * (1 + percentage_change * 0.01) + new_inhabitants
-  #   end
-    capitalise_letter = fn index
-      # capitalised_word = word.capitalise_at(index)
-      capitalised_word = word
+    codepoints = String.codepoints(word)
+
+    capitalise_letter = fn index ->
+      letter = Enum.at(codepoints, index)
+      List.replace_at(codepoints, index, String.upcase(letter)) |> Enum.join()
     end
 
-    Enum.to_list(0..String.length(word))
+    Enum.to_list(0..(String.length(word) - 1))
     |> Enum.map(capitalise_letter)
-
   end
 end
